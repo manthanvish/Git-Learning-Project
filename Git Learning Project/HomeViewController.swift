f177a9599fb08bfbf06a093e1080cd5a419385e7
Manthan <manthanvishwakarma2643@gmail.com>
2025-06-25 18:05:14 +0530

    Make home view controller

---
+++ Git Learning Project/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  Git Learning Project
+//
+//  Created by Manthan on 25/06/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func save(){
+        print("Saving....!!")
+    }
+   
+}
